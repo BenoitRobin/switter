21c21ffbfcc63f595f40f79c88c8e15ffd407f43
BenoitRobin <36993553+BenoitRobin@users.noreply.github.com>
2021-01-06 11:02:24 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let name;
+</script>
+
+<main class="test">
+	<h1>Hello {name}!</h1>
+	<p class="test-p">Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style type="text/scss">
+	.test {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+
+		&-p {
+			width: 240px;
+			margin: auto;
+		}
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
